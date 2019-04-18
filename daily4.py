d654912601550e956d737d9e4e2b3fc7b9443a9d
PeterCassell92 <pc0122@my.bristol.ac.uk>
2019-04-18 01:13:50 +0100

    some more challenges

--- daily4.py
+++ daily4.py
@@ -7,3 +7,25 @@ Given an array of integers, find the first missing positive integer in linear ti
 For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 
 You can modify the input array in-place."""
+
+array = [3, 4,-1, 1]
+
+
+def findlowestpos(array):
+	sortedarray = sorted(array)
+	i=0 #default value if no positive integers in entire array
+	print(sortedarray)
+	for elem in sortedarray:
+		if elem >=0:
+			x= elem
+			i = sortedarray.index(elem)
+			break
+
+	while (i+1) != len(sortedarray) and sortedarray[i] +1 == sortedarray[i+1]:
+		i += 1
+
+	return (sortedarray[i]+1)
+
+
+print(findlowestpos(array))
+print(findlowestpos([1,2,0]))
