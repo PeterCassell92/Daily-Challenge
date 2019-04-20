3be3bf08a5fca3d6e5fe6acdb67f4b2748f83fd4
PeterCassell92 <pc0122@my.bristol.ac.uk>
2019-04-20 18:37:28 +0100

    up to challenge 8

--- daily7.py
+++ daily7.py
@@ -10,14 +10,6 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 import unittest
 
-class TestOutput(unittest.TestCase):
-	@unittest.skip("practicing skip")
-	def testskip():
-		print("Nothing to see here")
-
-	def testnumpossibles(message1):
-		self.assertEqual(numpossibles(message1), 3, "unittest says it should be 3")
-
 
 def numpossibles(message):
 
@@ -41,11 +33,12 @@ def numpossibles(message):
 					doublejump = True
 			possiblejumps.append((singlejump,doublejump))
 			a += 1
-		print(possiblejumps)
+		#print(possiblejumps)
 		return possiblejumps
 
 	possiblejumps = getpossiblejumps(message)
 
+	#jump function makes valid jump functions until there are no valid jumps or the final jump would reach the end of the total string.
 	def jump(messagelength, index , numcompletedpaths):
 		k = index
 		x = numcompletedpaths
@@ -61,8 +54,13 @@ def numpossibles(message):
 				x= jump(messagelength, k+2, x)
 		return x
 	
-	x= jump(len(message), 0, 0)
+	x= jump(len(message), 0, 0) # starts at 0th index of String and with 0 completed paths.
 	return x
+
+#a test function to ensure program is running as expected.
+def outputtest(message, expectedoutput):
+	assert numpossibles(message) == expectedoutput , ("should be " + str(expectedoutput))
+
 
 if __name__ == '__main__':
 	message1 = "111"
@@ -71,11 +69,19 @@ if __name__ == '__main__':
 	message4 = "2010213262110"
 	message5 = "1111111"
 	message6 = "11111111"
-	#assert numpossibles(message1) == 3, "should be 3"
-	#unittest.main()
-	print(numpossibles(message1))
-	print(numpossibles(message2))
-	print(numpossibles(message3))
-	print(numpossibles(message4))
-	print(numpossibles(message5))
-	print(numpossibles(message6))
+
+	print(str(message1) + " has " +str(numpossibles(message1)) + " possible unique decodes")
+	print(str(message2) + " has " +str(numpossibles(message2)) + " possible unique decodes")
+	print(str(message3) + " has " +str(numpossibles(message3)) + " possible unique decodes")
+	print(str(message4) + " has " +str(numpossibles(message4)) + " possible unique decodes")
+	print(str(message5) + " has " +str(numpossibles(message5)) + " possible unique decodes")
+	print(str(message6) + " has " +str(numpossibles(message6)) + " possible unique decodes")
+
+	outputtest(message1, 3)
+	outputtest(message2, 3)
+	outputtest(message3, 6)
+	outputtest(message4, 12)
+	outputtest(message5, 21)
+	outputtest(message6, 34)
+
+	
--- daily8.py
+++ daily8.py
@@ -11,7 +11,71 @@ For example, the following tree has 5 unival subtrees:
    0
   / \
  1   0
-    / \
+	/ \
    1   0
   / \
  1   1"""
+
+ #CURRENT VERSINO STILL BUGGY. WORK IN PROGRESS
+import copy
+
+class Node (object):
+	numunival = 0 # this is a static class variable.
+	nodedesignation = 0
+	def __init__(self, value):
+		self.value = value
+		self.right = None
+		self.left = None
+		Node.nodedesignation +=1
+		self.designation = copy.deepcopy(Node.nodedesignation)
+		print (self.designation)
+
+	def addrightNode(self, value):
+		self.right = Node(value)
+
+	def addleftNode(self, value):
+		self.left = Node(value)	
+
+	def find_unival_trees(self):
+		return self.unival_helper(self.value)
+
+	def unival_helper(self, value):
+		if self.right == None and self.left == None:
+			print("HIT FROM NODE " + str(self.nodedesignation) + "_WITH NO CHILD NODES")
+			Node.numunival +=1
+			return True
+
+		if self.value == value:
+			print("test")
+			if (self.left.unival_helper(value) and self.right.unival_helper(value)) == True :
+				print("HIT FROM NODE " + str(self.nodedesignation) + "_WITH TWO CHILD NODES")
+				Node.numunival +=1
+				return True
+			if (self.left == None and self.right.unival_helper(value)) == True:
+				print("HIT FROM NODE " + str(self.nodedesignation) + "_ WITH ONLY RIGHT CHILD NODE")
+				Node.numunival +=1
+				return True
+			if (self.right == None and self.left.unival_helper(value)) == True:
+				print("HIT FROM NODE" + str(self.nodedesignation) + "_WITH ONLY LEFT CHILD NODE")
+				Node.numunival +=1 
+				return True
+		else:
+			if self.right != None:
+				print("is happening RIGHT")
+				self.right.unival_helper(self.right.value)
+			if self.left != None:
+				return self.left.unival_helper(self.left.value)
+				print("is happening LEFT")
+		return False
+
+#generating the tree from the example
+tree = Node('0')
+tree.addleftNode('1')
+tree.addrightNode('0')
+tree.right.addrightNode('0')
+tree.right.addleftNode('1')
+tree.right.left.addleftNode('1')
+tree.right.left.addrightNode('1')
+
+print(tree.find_unival_trees())
+print(tree.numunival)
